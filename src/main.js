6972d5bf33e4ec78ef2af01f10a091518bbb8ef1
MinmusLin <minmuslin@outlook.com>
2024-11-27 15:36:59 +0800

    Update axios

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
 import store from '@/store'
+import axios from 'axios'
 import '@/element-ui'
+
+axios.defaults.baseURL = 'http://110.42.214.164:8005/'
+axios.defaults.headers.post['Content-Type'] = 'application/json'
+
+Vue.prototype.$axios = axios
 
 new Vue({
     router,
